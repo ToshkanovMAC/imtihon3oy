ad385c6faf98f0103839b8d6e2a8be82ab9c32ce
ulug'bek <toshkanov.4321icloud.com@ulugbeks-MacBook-Air.local>
2025-12-15 20:29:15 +0500

    imtihon

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1-masala
+// let a = prompt("ves kiriting");
+// if (a > 90) {
+//     alert("dieta qiling");
+// }else if (a < 70) {
+//     alert("koproq yeng");
+// }else {
+//     alert("yaxshi");
+// }
+
+// 2-masala
+// let n = 6;
+// let b = (n - 2) * n / 2;
+// console.log(b);
